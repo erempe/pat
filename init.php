eefb8cf039b9e901feadb51275b8875974255c62
RMP <rmp8427@gmail.com>
2012-01-15 16:58:29 +0100

    Added init procedure
    + init.php for initializing PAT
    - TPipeline due to unknown body of TPipeline::pipe
    # updates README with hint for used PHP Version

---
+++ init.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace pat;
+
+\set_include_path(get_include_path() . PATH_SEPARATOR
+        . realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..'));
+
+function __autoload($class)
+{
+    return ((include $class . '.php') === 'OK');
+}
+
+\spl_autoload_register('pat\__autoload');
+
+?>
