94a4c2034f12a57300a687aa004d29a30f9fd90b
RMP <rmp8427@gmail.com>
2012-01-27 20:43:05 +0100

    Added error handling to init of pat

--- init.php
+++ init.php
@@ -8,9 +8,15 @@ namespace pat;
 
 function __autoload($class)
 {
+    $class = str_replace('\\', '/', $class);
     return ((include $class . '.php') === 'OK');
 }
 
-\spl_autoload_register('pat\__autoload');
+$success = \spl_autoload_register('pat\__autoload');
+
+if (false == $success)
+{
+    throw new \ErrorException('Could not register pat\__autoload to autoloader');
+}
 
 ?>
