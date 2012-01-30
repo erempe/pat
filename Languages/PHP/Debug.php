99491fa60a5e7a43759249122bf6a2e69133f5c5
RMP <rmp8427@gmail.com>
2012-01-30 20:25:20 +0100

    Added dump to Debug-Helper

--- Languages/PHP/Debug.php
+++ Languages/PHP/Debug.php
@@ -5,8 +5,24 @@ namespace pat\Languages\PHP;
 class Debug {
 
     public static function init() {
-        ini_set('display_errors', true);
-        error_reporting(E_ALL);
+        \error_reporting(-1);
+        \ini_set('display_errors', true);
+        \ini_set('display_errors_startup', true);
+    }
+    
+    public static function dump()
+    {
+        echo '<pre>';
+        \ob_start();
+        $success = \call_user_func_array('var_dump', \func_get_args());
+        
+        if (false === $success)
+        {
+            throw new \ErrorException('Can not dump variables');
+        }
+        
+        echo \htmlspecialchars(\ob_get_clean());
+        echo '</pre>';
     }
 
 }
